11d7f8b1dbda9aff15c8125a6f5bcb7adde50aaf
Asif Mehedi <asifm.inbox@gmail.com>
2026-01-28 07:20:18 -0500

    Remove references bubble charts

--- pages/data-highlights/rise-of-asia/index.vue
+++ pages/data-highlights/rise-of-asia/index.vue
@@ -9,9 +9,6 @@ import {
   sectorsObj,
   regionSectorDataProm,
   yearRegionDataProm,
-  // TODO
-  // company1980to89DataProm,
-  // company2007to16DataProm,
 } from '@/assets/js/fetchData.js';
 
 const regionList = ['Asia Pacific', 'Europe', 'North America'];
@@ -144,36 +141,6 @@ export default {
       });
     });
 
-    // company2007to16DataProm.then(data_ => {
-    //   Object.keys(regionObj).forEach(region => {
-    //     const seriesData = data_
-    //       .filter(row => row.region === region)
-    //       .map(row => ({
-    //         name: row.company,
-    //         value: +row.total_count_patents,
-    //       }));
-    //     vm.regionCompanyPack2007to16Opts.series.push({
-    //       name: region,
-    //       data: seriesData,
-    //       color: regionObj[region],
-    //     });
-    //   });
-    // });
-    // company1980to89DataProm.then(data_ => {
-    //   Object.keys(regionObj).forEach(region => {
-    //     const seriesData = data_
-    //       .filter(row => row.region === region)
-    //       .map(row => ({
-    //         name: row.company,
-    //         value: +row.total_count_patents,
-    //       }));
-    //     vm.regionCompanyPack1980to89Opts.series.push({
-    //       name: region,
-    //       data: seriesData,
-    //       color: regionObj[region],
-    //     });
-    //   });
-    // });
     yearRegionDataProm.then(data_ => {
       regionList.forEach(region => {
         const filteredData = data_
@@ -211,16 +178,6 @@ export default {
       div(class="uk-width-1-1@s")
         p.uk-margin-large-top It's notable, but perhaps not surprising, that the Asia Pacific region outshines North America in manufacturing — and is not far behind in business equipment and software, the sector that generated most patents during 1980–2016.
         chart-heatmap(:custom-options="regionSectorHeatmapOpts")
-    //- .uk-grid(uk-grid).uk-card-default.uk-padding
-    //-   div(class="uk-width-1-1@s")
-    //-     p(class="uk-hidden@s") [Please view the charts on a larger screen. They cannot be rendered on small screens.]
-    //-     p.uk-margin-large-top Here's another look at how regional distribution changed dramatically between the first ten years (1980–2016) and the last ten years (2007–2016) of our dataset's coverage.
-    //-     p The smaller circles represent the top 50 companies, by aggregate count of patents, during those two periods. Hover over the circles for details.
-      //- div(class="uk-width-1-1@s uk-visible@s" uk-grid).uk-grid
-      //-   div(class="uk-width-1-2@s")
-      //-     chart-circlepack(:custom-options="regionCompanyPack1980to89Opts")
-      //-   div(class="uk-width-1-2@s")
-      //-     chart-circlepack(:custom-options="regionCompanyPack2007to16Opts")
 </template>
 
 <style lang="scss" scoped>
--- pages/data-highlights/tech-leading-innovation/index.vue
+++ pages/data-highlights/tech-leading-innovation/index.vue
@@ -1,39 +1,19 @@
 <script>
 import { format as d3Format } from 'd3';
-// import { bubbleRnDPatentOpts } from '@/assets/js/chartBubbleRnDPatent.js';
 
 import ChartStacked from '@/components/outputs/ChartStacked';
-import ChartBubble from '@/components/outputs/ChartBubble';
 
-// import { lists } from '@/assets/data/listData.js';
 import {
   sectorsObj,
   yearSectorDataProm,
-  // TODO
-  // company1980to89DataProm,
-  // company2007to16DataProm,
 } from '@/assets/js/fetchData.js';
 
 export default {
   components: {
     ChartStacked,
-    ChartBubble,
   },
   data() {
     return {
-      numBubbles: 30,
-      company1980to89BubbleOpts: {
-        series: [],
-        title: {
-          text: 'R&D vs. Patent Count: Top 30 Companies (1980–1989)',
-        },
-      },
-      company2007to16BubbleOpts: {
-        series: [],
-        title: {
-          text: 'R&D vs. Patent Count: Top 30 Companies (2007–2016)',
-        },
-      },
       yearSectorColumnOpts: {
         plotOptions: {
           series: {
@@ -80,47 +60,6 @@ export default {
   beforeCreate() {
     const vm = this;
 
-    //     company1980to89DataProm.then(data_ => {
-    //       Object.keys(sectorsObj).forEach(sector_code => {
-    //         const sectorData = data_
-    //           .slice(0, this.numBubbles)
-    //           .filter(row => row.sector_code == sector_code)
-    //           .map(row => ({
-    //             x: +row.total_rdex,
-    //             y: +row.total_count_patents,
-    //             z: +row.total_sales,
-    //             capex: +row.total_capex,
-    //             ebitda: +row.total_ebitda,
-    //             company: row.company,
-    //             sector: row.sector,
-    //           }));
-
-    //         vm.company1980to89BubbleOpts.series.push({
-    //           data: sectorData,
-    //           name: sectorsObj[sector_code],
-    //         });
-    //       });
-    //     });
-    //     company2007to16DataProm.then(data_ => {
-    //       Object.keys(sectorsObj).forEach(sector_code => {
-    //         const sectorData = data_
-    //           .slice(0, this.numBubbles)
-    //           .filter(row => row.sector_code == sector_code)
-    //           .map(row => ({
-    //             x: +row.total_rdex,
-    //             y: +row.total_count_patents,
-    //             z: +row.total_sales,
-    //             capex: +row.total_capex,
-    //             ebitda: +row.total_ebitda,
-    //             company: row.company,
-    //             sector: row.sector,
-    //           }));
-    //         vm.company2007to16BubbleOpts.series.push({
-    //           data: sectorData,
-    //           name: sectorsObj[sector_code],
-    //         });
-    //       });
-    //     });
     yearSectorDataProm.then(data_ => {
       Object.keys(sectorsObj).forEach(sector_code => {
         const filteredData = data_
@@ -135,10 +74,6 @@ export default {
       });
     });
   },
-  // created() {
-  //   Object.assign(this.company1980to89BubbleOpts, bubbleRnDPatentOpts);
-  //   Object.assign(this.company2007to16BubbleOpts, bubbleRnDPatentOpts);
-  // },
 };
 </script>
 
@@ -160,13 +95,5 @@ export default {
         p.uk-margin-large-top The chart above does not capture the fast growth in annual patent grants during this period. When put in that context, as is done in the streamgraph here, the surge of patents in the technology sector is even more striking. 
       div(class="uk-width-2-3@s")
          chart-stacked(:custom-options="yearSectorStreamOpts") 
-    //- .uk-grid(uk-grid).uk-card-default.uk-padding
-    //-   div(class="uk-width-1-3@s")
-    //-     p(class="uk-hidden@s") [Please view the charts on a larger screen. They cannot be rendered on small screens.]
-    //-     p.uk-margin-large-top The two bubble charts show the top 30 companies, by total count of patents over ten years, of two very different periods: 1980–1989 and 2007–2016. The periods represent the first ten and last ten years of our dataset's coverage. 
-    //-     p Today's top companies generate an order of magnitude more patents than those of 1980s. We cannot directly compare R&amp;D expenditure between these two periods without taking the changing value of US$ into account. It, however, appears that the growth in patents over time is faster than the growth in R&amp;D expenditure.
-      //- div(class="uk-width-2-3@s uk-visible@s")
-      //-    chart-bubble(:custom-options="company1980to89BubbleOpts") 
-      //-    chart-bubble(:custom-options="company2007to16BubbleOpts") 
 
 </template>

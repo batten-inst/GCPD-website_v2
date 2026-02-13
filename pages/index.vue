1f575a3cc46d02d8063c881e640380c0b5b2f3c5
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-13 16:03:06 -0500

    Major update of home page -- content and style

--- pages/index.vue
+++ pages/index.vue
@@ -14,34 +14,61 @@ export default {
 </script>
 
 <template lang="pug">
-.uk-section.uk-animation-slide-top-small.uk-padding-small#main-section
+.uk-section.uk-animation-slide-top-small.uk-padding-small.bg-blue-fade-out-9
   .uk-container.uk-container-large.uk-padding-remove-top
     div(class="uk-grid uk-child-width-1-2@s")
       div
         .uk-card.uk-card-body
           h2.uk-h2.uk-margin-remove.fg-orange-700 UVA Darden
           h1.my-text-heavy.uk-heading-medium.uk-margin-remove.fg-blue-500 Global Corporate Patent Dataset
+          p.uk-margin-remove <i>Linking 5.3 million USPTO patents to 25 thousand publicly listed companies worldwide</i>
           div
             div(class="uk-column-1-2@m")
-              p.fg-orange-900.uk-margin-medium-top.uk-dropcap.uk-text-large.uk-column-span Corporate innovation plays a crucial role in pushing the frontier of technology — and technology's progress helps advance economic prosperity. By tracking corporate innovation, we can learn much about where and how new ideas, knowledge, products and techniques are emerging.
-              p.uk-margin-remove-top The new dataset available here provides a window into the world of corporate patents — an important indicator of corporate research and development activities and the consequent innovation—across sectors and countries.
-              p.uk-margin-remove-top This website has been updated to reflect the new Global Corporate Patent Dataset (GCPD) v2, which now covers an expanded set of companies, patents, and countries over a broader time frame.
-              p The public release of this dataset will hopefully open up new research possibilities and enable the discovery of insights in different areas of finance and economics. This data will also help advance public discussions, outside of academia, on corporate innovation, productivity and performance.
+              p.fg-orange-900.uk-margin-medium-top.uk-dropcap.uk-text-lead.uk-column-span Corporate innovation plays a crucial role in pushing the frontier of technology — and technology's progress helps advance economic prosperity. By tracking corporate innovation, we can learn much about where and how new ideas, knowledge, products and techniques are emerging.
+              p.uk-margin-remove-top The dataset available here provides a window into that world of corporate patents — an important indicator of corporate research and development activities and the consequent innovation over time and across sectors and regions.
+              
+              p We hope that the public release of this dataset will open up new research possibilities and enable the discovery of insights in different areas of finance and economics. This data will also help advance public discussions, outside of academia, on corporate innovation, productivity and performance.
               nuxt-link(to="/interactive")
                 img.uk-box-shadow-small.uk-box-shadow-hover-medium.uk-border-rounded.uk-margin-medium-top.uk-animation-slide-top(aria-hidden="true" src="/img/worldmap.png" alt="World map" @click="loading = !loading")
               p.uk-text-small [Click on the map to interactively explore the dataset. It may take a few seconds to load all data.]
               div(v-if="loading" uk-spinner="ratio:5").uk-position-center.uk-position-absolute
       
-      div.uk-margin-large-top
+      div.uk-margin-large-top.uk-padding
         .uk-card.uk-card-body.uk-margin.bg-orange-50
           h2.uk-h2.fg-blue About the Data
-          div(class="uk-column-1-2@l")
-            p The Global Corporate Patent Dataset links 5.3 million patents awarded by the U.S. Patent and Trademark Office (USPTO), between January 1976 and September 2023, to 25 thousand publicly listed firms across 99 countries (by the location of parent companies’ headquarters).
-            p A detailed description of the dataset, including its key features and how it was constructed, can be found in <a href="/documents/DataConstructionDetails_v2.pdf"> this document</a>. 
-            p Please visit the other sections of this website to explore the breadth of the dataset's coverage — across countries and sectors over time. 
-            button.uk-button.uk-button-primary.uk-link-reset: nuxt-link(to="/get-data") Get Data
+          div
+            p.uk-padding-small.bg-orange-100 The Global Corporate Patent Dataset (v2) links, using sophisticated matching techniques, about <b>5.3 million patents</b> — awarded by the U.S. Patent and Trademark Office (USPTO) between January 1976 and September 2023 — to <b>25 thousand publicly listed companies</b> across 99 countries (as identified by the location of parent companies’ headquarters).
+
+            p.uk-margin-remove Originally released in 2019, the dataset has been significantly updated and expanded in 2026.
+
+            table.uk-table.uk-table-divider.comparison-table.uk-text-small.uk-margin-remove(class="uk-width-4-5@l")
+              thead
+                tr
+                  th 
+                  th.fg-orange-900 GCPD v1 (2019)
+                  th.fg-orange-900 GCPD v2 (2026)
+              tbody
+                tr
+                  td.fg-orange-900 Years covered
+                  td 1980 – 2017
+                  td 1976 – Sep 2023
+                tr
+                  td.fg-orange-900 Number of patents
+                  td 3.1 Million
+                  td 5.3 Million
+                tr
+                  td.fg-orange-900 Number of companies
+                  td 9.2 Thousand
+                  td 25 Thousand
+                tr
+                  td.fg-orange-900 Countries represented
+                  td 50
+                  td 99
+
+            p Please consult <a href="/documents/DataConstructionDetails_v2.pdf">this document</a> for a detailed description of the dataset, including its key features and how it was constructed. 
+            button.uk-button.uk-button-secondary.uk-link-reset: nuxt-link(to="/get-data") Get Data
       
-        .uk-card.uk-card-body.uk-tile-muted
+        .uk-card.uk-card-body.uk-tile-muted.uk-padding-remove-top
           h3.uk-h3 Understanding Innovation Through Patent Data
           p <strong>Pedro Matos</strong>, professor at the University of Virginia Darden School of Business, and <strong>Jan Bena</strong>, professor at the University of British Columbia Sauder School of Business, sat down with the Batten Institute's <strong>Erika Herz</strong> to talk about this dataset.
           p They described important aspects of the data and explained how this resource can benefit scholars who seek to understand corporate innovation.
@@ -50,28 +77,25 @@ export default {
 
 </template>
 
-<style lang="scss" scoped>
-#main-section {
-  min-height: 80vh;
-  background-color: #e9f0fc;
+<style lang="scss">
+.uk-section {
+  min-height: 85vh;
 }
+
 audio {
   max-width: 300px;
   object-fit: inherit;
 }
+
+.comparison-table th,
+.comparison-table td {
+  padding: 6px 8px;
+}
+
 @media (max-width: 600px) {
   audio {
     max-width: 150px;
     object-fit: inherit;
-  }
-  .uk-padding-small {
-    padding: 0%;
-  }
-  .uk-container {
-    padding: 0%;
-  }
-  .uk-margin-large-top {
-    margin: 0%;
   }
 }
 </style>

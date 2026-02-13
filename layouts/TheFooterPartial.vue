403cb5a0a7dbd6aa3e796eb4fb13d96defac1103
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-13 16:02:47 -0500

    Style update

--- layouts/TheFooterPartial.vue
+++ layouts/TheFooterPartial.vue
@@ -9,7 +9,7 @@ footer
       |  at 
       a.fg-white(href="http://www.darden.virginia.edu/" target="_blank" rel="noreferrer") the University of Virginia Darden School of Business
       |  as part of its long-term support to the project.
-  .uk-section.uk-padding-small.uk-text-center.bg-blue-700.fg-white
+  .uk-padding-small.uk-text-center.bg-blue-700.fg-white
     p COPYRIGHT © 2026 THE RECTOR AND VISITORS OF THE UNIVERSITY OF VIRGINIA. ALL RIGHTS RESERVED.
 </template>
 

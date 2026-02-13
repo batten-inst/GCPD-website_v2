0bdde3491b438084c5df0642d9bd1083934f0897
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-13 16:02:33 -0500

    Update about us page

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -6,23 +6,28 @@ export default {
         {
           name: 'Jan Bena',
           title:'Associate Professor, Bank of Montreal Chair in International Finance, University of British Columbia Sauder School of Business <br>Batten Research Fellow, UVA Darden School of Business',
-          email: 'Jan.bena@sauder.ubc.ca',
+          personalWebsite: 'https://www.janbena.com/',
         },
         {
           name: 'Miguel A. Ferreira',
           title: 'BPI Fundação la Caixa Chair in Responsible Finance, Nova School of Business and Economics',
-          email: 'miguel.ferreira@novasbe.pt',
+          personalWebsite: 'https://www.miguelferreirafinance.com/',
         },
         {
           name: 'Pedro Matos',
           title: 'James A. and Stacy Cooper Bicentennial Professor of Business Administration, UVA Darden School of Business',
-          email: 'matosp@darden.virginia.edu',
+          personalWebsite: 'https://www.darden.virginia.edu/faculty-research/directory/pedro-matos',
         },
         {
           name: 'Pedro Pires',
-          title: 'Research Fellow, Nova School of Business and Economics',
-          email: 'pedro.pires@novasbe.pt',
+          title: 'Assistant Professor of Economics, Nova School of Business and Economics',
+          personalWebsite: 'https://www.pedropires.io/',
         },
+        {
+          name: 'David Barkemayer',
+          title: 'Research Fellow, University of British Columbia Sauder School of Business',
+          personalWebsite: 'https://www.linkedin.com/in/david-barkemeyer/',
+        }
       ],
     };
   },
@@ -32,26 +37,22 @@ export default {
 <template lang="pug">
 .uk-section.uk-animation-slide-top-small
   .uk-container.uk-container-exapnd
-    main(uk-grid class="uk-grid uk-child-width-1-2@s")
+    main(uk-grid class="uk-grid uk-child-width-1-2@m")
       div
-        .uk-card.uk-card-default.uk-card-body
-          h1.uk-heading-small.fg-blue The Project Team
+        div.bg-blue-50.uk-card-body
+          h2.uk-heading-small.fg-blue The Project Team
           p <span class="uk-text-bold">The UVA Darden Global Corporate Patent Dataset</span> is the result of a multiyear collaboration among the following academic scholars. 
-          p If you have any questions, suggestions or comments, please contact us at <a href="mailto:GCPD@darden.virginia.edu">GCPD@darden.virginia.edu</a>.
           dl(v-for="(member, i) in members" :key="i").uk-width-1-1.uk-description-list
-            dt.my-text-heavy {{ member.name }}
+            dt.my-text-heavy <a :href="member.personalWebsite" target="_blank">{{ member.name }}</a>
             dd(v-html="member.title")
             
-      
       div 
-        .uk-card.uk-card-body.uk-card-default#batten
-          h1.fg-orange-900 Batten Institute for Entrepreneurship, Innovation and Technology
-          p The Batten Institute for Entrepreneurship, Innovation and Technology at the UVA Darden School of Business provided institutional support to this collaboration. The Institute's research initiative advances knowledge that addresses real-world challenges and shapes Darden's curriculum, and its educational offerings are ranked top 10 in the world. The Batten Institute was established by the vision and philanthropy of UVA alumnus Frank Batten Sr. 
-          
-          a.uk-button.uk-link-reset.uk-button-default(href="http://www.darden.virginia.edu/batten-institute/" target="_blank") Learn more
+        div.bg-orange-50.uk-card-body
+          h2.fg-orange-900 Batten Institute for Entrepreneurship, Innovation and Technology
+          p The Batten Institute, at the UVA Darden School of Business, provided institutional support to this collaboration. The Institute's research initiative advances knowledge that addresses real-world challenges and shapes Darden's curriculum, and its educational offerings are ranked among the best in the world. The Batten Institute was established by the vision and philanthropy of UVA alumnus Frank Batten Sr. <a href="http://www.darden.virginia.edu/batten-institute/" target="_blank">Learn more</a>
         
-          div.uk-margin-medium-top.uk-align-right
+          div
             img(alt="Darden School of Business" src="/img/darden-saunders.jpg")
-            .uk-text-center.uk-margin-remove.uk-text-small.uk-lightbox-caption Darden School of Business at the University of Virginia
+            .uk-margin-remove.uk-text-meta.uk-lightbox-caption Darden School of Business at the University of Virginia
         
 </template>

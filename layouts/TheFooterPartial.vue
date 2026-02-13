0d60edcc7e73e0877cd9483059cf488d67ff371d
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-13 13:32:33 -0500

    Simplify footer

--- layouts/TheFooterPartial.vue
+++ layouts/TheFooterPartial.vue
@@ -1,18 +1,14 @@
 <template lang="pug">
 footer
-  .uk-padding-small.bg-blue-50.uk-text-center.uk-margin-remove
+  .uk-padding-small.bg-blue-50.uk-text-center.uk-margin-remove.fg-white.bg-blue-400
     p If you have comments or questions about this dataset, please email us at 
-      a(href="mailto:GCPD@darden.virginia.edu")
-        span GCPD@darden.virginia.edu.
-
-    div
-      p This website was developed by <span class="uk-text-bold"> #[a(href="https://www.darden.virginia.edu/batten-institute/" target="_blank" rel="noreferrer") the Batten Institute for Entrepreneurship, Innovation and Technology]</span> at #[a(href="http://www.darden.virginia.edu/" target="_blank" rel="noreferrer") the University of Virginia Darden School of Business] as part of its long-term support to the project. 
-      div.my-text-thin.uk-text-small
-        .uk-icon-button(uk-icon="icon: mail")
-        a(href="mailto:Batten@darden.virginia.edu").uk-margin-small-right Batten@darden.virginia.edu
-        .uk-icon-button(uk-icon="icon: twitter")
-        a(href="https://www.twitter.com/BattenInstitute" target="_blank" rel="noreferrer") @BattenInstitute
-   
+      a.fg-white(href="mailto:GCPD@darden.virginia.edu") GCPD@darden.virginia.edu
+      | . This website was developed by 
+      span.uk-text-bold
+        a.fg-white(href="https://www.darden.virginia.edu/batten-institute/" target="_blank" rel="noreferrer") the Batten Institute for Entrepreneurship, Innovation and Technology
+      |  at 
+      a.fg-white(href="http://www.darden.virginia.edu/" target="_blank" rel="noreferrer") the University of Virginia Darden School of Business
+      |  as part of its long-term support to the project.
   .uk-section.uk-padding-small.uk-text-center.bg-blue-700.fg-white
     p COPYRIGHT © 2026 THE RECTOR AND VISITORS OF THE UNIVERSITY OF VIRGINIA. ALL RIGHTS RESERVED.
 </template>

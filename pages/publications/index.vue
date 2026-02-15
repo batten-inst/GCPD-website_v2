5798b98b9259cdd076f95579110bdc750db4a4c4
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-15 18:33:48 -0500

    Update publications page layout and styling

--- pages/publications/index.vue
+++ pages/publications/index.vue
@@ -29,16 +29,16 @@ export default {
   .uk-container(class="uk-width-1-2@xl uk-width-3-4@l")
     h1.uk-heading-small GCPD in Research Publications
     p.uk-text-lead Since GCPD's first use in a 2017 paper in a leading finance journal (JFE), it has been used in other top publications — as seen in this sample list of papers in economics (JPE), accounting (JAR) and strategy (SMJ).
-    p 
+    p Read <a href="/documents/UVA-Darden-Research-Briefing.pdf" target="_blank">a UVA Darden Research Briefing</a> based on the original paper. 
     .uk-grid.uk-grid(uk-grid).uk-grid-large(class="uk-child-width-1-2@s")
       div(v-for="(pub, i) in publications" :key="i")
-        div.bg-orange-50
+        div(:class="i === 0 ? 'bg-blue-50' : 'bg-orange-50'")
           .uk-card-body
             .uk-h6
               span.fg-blue {{ pub.publication }} 
               span.fg-orange-700 {{ pub.year }}
-            img.uk-width-small.uk-margin-bottom(:src="pub.imgSrc" :alt="pub.publication").uk-float-right.uk-margin-left
-            p.uk-text-large.fg-blue-700.uk-margin-remove {{ pub.title }}
+            img.uk-width-small(:src="pub.imgSrc" :alt="pub.publication").uk-float-right
+            p.uk-text-large {{ pub.title }}
             p.uk-text-bold.uk-margin-remove {{ pub.authors }}
 
             p.uk-text-small.fg-blue Abstract: “{{ pub.abstract }}”

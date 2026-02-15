cf2f152693a5a8e4ed1c31b45f4c19fc719326cf
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-15 17:31:54 -0500

    Change survey from qualtrics to google forms

--- pages/get-data/index.vue
+++ pages/get-data/index.vue
@@ -12,5 +12,6 @@ export default {
   .uk-container.uk-container-small
     .uk-card.uk-card-default.uk-card-body.uk-box-shadow-small
       get-data-text
-      <iframe src="https://virginia.az1.qualtrics.com/jfe/form/SV_9B2ivuARIUgDgC9" height="1000px" title="Short form before download"></iframe>
+    .uk-container.uk-container-small.uk-margin-large-top
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSerZc3kWlpCRYTvCjI8P2PVRTh11SNS1l3-tGicR2FQo-Fvpg/viewform?embedded=true" width="400" height="1300" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 </template>

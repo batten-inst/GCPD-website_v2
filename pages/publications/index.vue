523dd6d628408698dab724818a7a226181f2f823
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-17 17:59:40 -0500

    Update publications

--- pages/publications/index.vue
+++ pages/publications/index.vue
@@ -14,7 +14,9 @@ export default {
       'Journal of Political Economy': '/img/jpe-cover.png',
       'Journal of Accounting Research': '/img/joar-cover.webp',
       'Strategic Management Journal': '/img/smj-cover.jpg',
+      'UVA Darden Research Briefing': '/img/darden-briefing-cover.png',
     };
+
 
     this.publications = data.map(pub => ({
       ...pub,
@@ -28,17 +30,16 @@ export default {
 .uk-section.uk-animation-slide-top-small
   .uk-container(class="uk-width-1-2@xl uk-width-3-4@l")
     h1.uk-heading-small GCPD in Research Publications
-    p.uk-text-lead Since GCPD's first use in a 2017 paper in a leading finance journal (JFE), it has been used in other top publications — as seen in this sample list of papers in economics (JPE), accounting (JAR) and strategy (SMJ).
-    p Read <a href="/documents/UVA-Darden-Research-Briefing.pdf" target="_blank">a UVA Darden Research Briefing</a> based on the original paper. 
-    .uk-grid.uk-grid(uk-grid).uk-grid-large(class="uk-child-width-1-2@s")
-      div(v-for="(pub, i) in publications" :key="i")
-        div(:class="i === 0 ? 'bg-blue-50' : 'bg-orange-50'")
-          .uk-card-body
+    div
+      p GCPD was first used in the following 2017 research paper, published in a leading finance journal. You can also read the 2018 UVA Darden Research Briefing, written for a general audience based on that paper.
+      div.uk-grid.uk-grid(uk-grid).uk-grid-large(class="uk-child-width-1-2@s").uk-padding-remove
+        div(v-for="(pub, i) in publications" :key="i" v-if="pub.highlight == '1'")
+          div.uk-card-body.bg-orange-50
             .uk-h6
               span.fg-blue {{ pub.publication }} 
               span.fg-orange-700 {{ pub.year }}
-            img.uk-width-small(:src="pub.imgSrc" :alt="pub.publication").uk-float-right
-            p.uk-text-large {{ pub.title }}
+            img.uk-width-small(:src="pub.imgSrc" :alt="pub.publication").uk-float-right.uk-margin-small-left
+            p.uk-text-large.uk-margin-small {{ pub.title }}
             p.uk-text-bold.uk-margin-remove {{ pub.authors }}
 
             p.uk-text-small.fg-blue Abstract: “{{ pub.abstract }}”
@@ -47,4 +48,20 @@ export default {
 
             button.uk-button.uk-button-secondary(v-if="pub.readMoreLink")
               a(:href="pub.readMoreLink" ).uk-link-reset Read More
+    div.uk-divider-icon
+    div
+      p The dataset has since been used in top publications in other fields — as seen in the following sample list of papers in economics (JPE), accounting (JAR) and strategy (SMJ).
+      p Click on a cover image to read more about that paper.
+      div(class="uk-width-2-3@s" v-for="(pub, i) in publications" :key="i" v-if="pub.highlight == 0").uk-card-default
+        div.uk-card-body.uk-margin-bottom.bg-blue-fade-out-9
+          a(:href="pub.readMoreLink" target="_blank").uk-link-reset
+            img.uk-width-small(:src="pub.imgSrc" :alt="pub.publication").uk-float-right.uk-margin-small-left
+          .uk-h6.uk-margin-remove
+            span.fg-blue {{ pub.publication }} 
+            span.fg-orange-700 {{ pub.year }}
+          p.uk-text-large.uk-margin-small {{ pub.title }}
+          p.uk-text-bold.uk-margin-remove {{ pub.authors }}
+          p.uk-text-small.fg-blue Abstract: “{{ pub.abstract }}”
+
+
 </template>

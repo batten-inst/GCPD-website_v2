75aff0f59937889543753bebe0297178197200e0
Asif Mehedi <asifm.inbox@gmail.com>
2026-02-18 21:55:09 -0500

    Update interactive text etc

--- pages/interactive/index.vue
+++ pages/interactive/index.vue
@@ -84,7 +84,7 @@ div.uk-section.uk-padding-remove-vertical.uk-margin-medium
           div(
             class="uk-width-1-4@m"
             data-step=2
-            data-intro="Select a region/country or a sector. <br><br> The data and visuals will change to reflect your selection."
+            data-intro="Select a region/country and a sector. <br><br> The data and visuals will change to reflect your selection."
             )
             div.uk-padding-small.uk-box-shadow-small.bg-orange-fade-out-9.uk-box-shadow-hover-medium
               select-parameter(
@@ -102,7 +102,7 @@ div.uk-section.uk-padding-remove-vertical.uk-margin-medium
           div(
             class="uk-width-1-2@m"
             data-step=3 
-            data-intro="Filter the data by years. Select a range of years or a single year."
+            data-intro="Filter the data by patent grant years. Select a range or a single year."
             )
             input-range(
               :key="yearsComponentKey"
@@ -112,7 +112,7 @@ div.uk-section.uk-padding-remove-vertical.uk-margin-medium
           div(
             class="uk-width-1-4@m"
             data-step=7 
-            data-intro="You can again start this guide from here. <br><br>At any point, you can set everything on the page to its beginning state using the reset button."
+            data-intro="You can show this guide again. <br><br>At any point, you can reset the data and map to its beginning state using the reset button."
             )
               button.my-text-tiny.uk-button.uk-button-small.uk-margin-small.bg-orange-100.fg-blue(
                 @click="startGuide"
@@ -122,19 +122,21 @@ div.uk-section.uk-padding-remove-vertical.uk-margin-medium
                 @click="resetData"
                 ) Reset Data &amp; Map
         hr    
-        p(class="uk-hidden@s").uk-padding-small This page works better on a larger screen. On a small screen, you can see other regions of the map by dragging it. 
+        p(class="uk-hidden@s").uk-padding-remove This page works better on a larger screen. On a small screen, you can see other regions of the map by dragging it. 
         .uk-card.uk-card-small.uk-width-1-1#map-card(
             data-step=4 
             data-intro="The circles on the map show total patent counts by country. To see the data for a country, hover over its circle. <br><br> Double click to zoom and drag to pan. Get back to full view using the reset button.<br><br> When the data changes, the circles rescale based on the then current minimum and maximum."
             )
-
-          p.uk-h5.uk-padding-tiny.uk-margin-remove Global ranking by countries' total patents in the selected sector(s) and year(s)
+          div.uk-padding-tiny
+            p.uk-h5.uk-margin-remove Countries ranked for the selected sector(s) and year(s)
+            div.uk-text-meta 
+              p.uk-margin-remove Click on a circle to see that country's top companies.
+              p.uk-margin-remove Ranking is within region if a region is selected. 
           
           map-with-circles.uk-align-center
           
-          //- .uk-card-footer
-          p.uk-margin-small.uk-text-meta.uk-padding-small
-            | Click on a circle to see that country's top companies. Circle sizes rescale based on selected data. You can zoom in by double clicking and pan by dragging the map. Use the reset button to get back to full view. See sector classification details <nuxt-link to="/data-highlights/note-sector-classification">here </nuxt-link>
+          p.uk-margin-small.uk-text-meta.uk-padding-tiny
+            | Circle sizes rescale based on selected data. You can zoom in by double clicking and pan by dragging the map. Use the reset button to get back to full view. See sector classification details <nuxt-link to="/data-highlights/note-sector-classification">here </nuxt-link>
  
       div(class="uk-width-1-4@m")
         compute-data
